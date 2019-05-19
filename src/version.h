2aeaa010034b86cf068f9f6a4571e5556958f267
Frajo Haider <f_haider@gmx.at>
2019-05-19 09:33:19 -0400

    bump plugin version

--- src/version.h
+++ src/version.h
@@ -27,8 +27,8 @@
 #define VERSION_H
 
 #define PLUGIN_NAME    "Mupen64Plus SDL Touch Input Plugin"
-#define PLUGIN_VERSION           0x020000
-#define INPUT_PLUGIN_API_VERSION 0x020001
+#define PLUGIN_VERSION           0x020500
+#define INPUT_PLUGIN_API_VERSION 0x020100
 #define CONFIG_API_VERSION       0x020100
 
 #define VERSION_PRINTF_SPLIT(x) (((x) >> 16) & 0xffff), (((x) >> 8) & 0xff), ((x) & 0xff)
